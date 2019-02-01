95d1462c2132f77d8bf4d515b89587bbb8eb97a2
Lorenzo <lmartini@student.ethz.ch>
2019-02-01 14:07:11 +0100

    Add script

---
+++ run_multi.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+## declare an array variable
+declare -a arr=(1000 10000 100000 1000000 10000000 100000000)
+declare -a pins=(0 4)
+## now loop through the above array
+for pin in "${pins[@]}"
+do
+	for i in "${arr[@]}"
+	do
+   		./target/release/benchmarker -i 1000000 -f $i -s 0 -r $pin > ./mergequeue_benchmark/"$i"Hz_0-"$pin".log
+	done
+done
