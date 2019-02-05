8a1b7ec5f2ed29a3bf72df073db4ecdbceb44df1
Lorenzo <lmartini@student.ethz.ch>
2019-02-05 10:49:57 +0100

    Edit runner

--- run_multi.sh
+++ run_multi.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 ## declare an array variable
-declare -a arr=(1000 10000 100000 1000000 10000000 100000000)
+declare -a arr=(10000 100000 1000000 10000000 100000000)
 declare -a pins=(1 4)
 ## now loop through the above array
 for pin in "${pins[@]}"
@@ -8,7 +8,7 @@ do
 	for i in "${arr[@]}"
 	do
 		sleep 10
-		echo ./target/release/benchmarker -i 1000000 -f $i -s 0 -r $pin > ./mergequeue_benchmark/"$i"Hz_0-"$pin".log
+		echo ./target/release/benchmarker -i 1000000 -f $i -s 0 -r $pin
    		./target/release/benchmarker -i 1000000 -f $i -s 0 -r $pin > ./mergequeue_benchmark/"$i"Hz_0-"$pin".log
 	done
 done
