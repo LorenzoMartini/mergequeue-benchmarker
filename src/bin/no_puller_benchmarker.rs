931ce1c0c410f8b907cbb6f81c99606709e72a30
Lorenzo <lmartini@student.ethz.ch>
2019-02-20 11:48:51 +0100

    Split threads

--- src/bin/no_puller_benchmarker.rs
+++ src/bin/no_puller_benchmarker.rs
@@ -7,6 +7,9 @@ use timely::communication::allocator::zero_copy::bytes_exchange::{MergeQueue, Si
 use timely::bytes::arc::Bytes;
 use mergequeue_benchmarker::config;
 use amd64_timer::ticks;
+use std::thread;
+use std::thread::sleep;
+use std::time::Duration;
 
 fn main() {
 
@@ -20,20 +23,23 @@ fn main() {
     let mut queue_send = queue.clone();
     let mut _queue_recv = queue.clone();
 
-    let mut hist = hdrhist::HDRHist::new();
-
-    let mut buffer = Bytes::from(vec![0u8; n_iterations * 2]);
-
-    mergequeue_benchmarker::utils::set_affinity(affinity_send);
-
-    for _ in 0..n_iterations {
-        let to_send = Some(buffer.extract_to(1));
-        let t0 = ticks();
-        queue_send.extend(to_send);
-        let t1 = ticks();
-        hist.add_value(t1 -t0);
-    }
-    // Collect and print measures using HDRHist
-    println!("Clock cycles for sender without receiver summary");
-    mergequeue_benchmarker::utils::print_hist_summary(hist);
+    let send = thread::spawn(move || {
+        let mut hist = hdrhist::HDRHist::new();
+        let mut buffer = Bytes::from(vec![0u8; n_iterations * 2]);
+        mergequeue_benchmarker::utils::set_affinity(affinity_send);
+        for _ in 0..n_iterations {
+            let to_send = Some(buffer.extract_to(1));
+            let t0 = ticks();
+            queue_send.extend(to_send);
+            let t1 = ticks();
+            hist.add_value(t1 - t0);
+        }
+        // Collect and print measures using HDRHist
+        println!("Clock cycles for sender without receiver summary");
+        mergequeue_benchmarker::utils::print_hist_summary(hist);
+    });
+    thread::spawn(move |queue_recv| {
+        sleep(Duration::from_millis(1));
+    });
+    send.join();
 }
